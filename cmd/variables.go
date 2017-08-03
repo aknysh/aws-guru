ac6321ca2622a2d27f245a9fca33a62bb140eecb
Rafal Wilinski <raf.wilinski@gmail.com>
2017-08-03 18:29:24 +0200

    Reorganize variables

--- cmd/variables.go
+++ cmd/variables.go
@@ -1,13 +1,23 @@
 package cmd
 
+/* Shared variables */
+var region string
+var profile string
+var cfgFile string
+
+/* ec2-snapshoter variables */
 var cronPattern string
 var cronName string
-var region string
-var accountId string
 var reattachOnly bool
-var cfgFile string
+
+/* iam-aim-linker variables */
+var accountId string
+var path string
+var slaveProfile string
+var slaveName string
+var format string
+
+/* vpc-setup variables */
 var vpcCidr string
 var privateSubnetCidr string
 var publicSubnetCidr string
-var profile string
-var format string
